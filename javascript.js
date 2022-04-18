ff30852f13a38735c8c7cd39ab747447052faef4
Oscar <oscarportillo721@gmail.com>
2022-04-18 14:55:16 -0300

    Finished working on the game logic. Game now works as intented when pressing each button. Function playRound() now saves the players' score and declares a winner. Added also a modal to pop-up whenever the player wins or loses. The modal has a button that restarts the game counter and the game can be played again from the start

--- javascript.js
+++ javascript.js
@@ -4,132 +4,207 @@ function computerPlay() {
 
     let item = items[Math.floor(Math.random()*items.length)];
 
-    console.log("Computer's choice = " + item);
+    /* console.log("Computer's choice = " + item); */
 
     return item;
 }
 
 
 const btns = document.querySelectorAll('#button');
+btns.forEach(btn => btn.addEventListener('click', playRound));
+
+const computer_points = document.querySelector('.computer');
+const player_points = document.querySelector('.player');
+const selection_text = document.querySelector('.selection-text');
+const label_player = document.querySelector('.player-point');
+const label_computer = document.querySelector('.computer-point');
+const scissors_card = document.querySelector('.scissors');
+const rock_card = document.querySelector('.rock');
+const paper_card = document.querySelector('.paper');
+
+console.log(rock_card, paper_card, scissors_card);
 
 
-let win = "Win!";
-let lose = "Lose!";
-let tie = "Tie!";
 
-function playRound(e, playerSelection, computerSelection) {
+let scorePlayer = 0;
+let scoreComputer = 0;
 
-    let select = this.classList.value;
 
-    console.log(" Player's selection = " + select);
 
-//let computer = computerPlay();
-playerSelection = select;
+function playRound(e, playerSelection) {
+
+// playerSelection = this.classList.value;
+
+playerSelection = e.target.className;
+
+let computerSelection = computerPlay();
+
 console.log(playerSelection);
-
-computerSelection = computerPlay();
 console.log(computerSelection);
+
+
+let win = "win";
+let lose = "lose";
+let tie = "tie";
 
 let input1 = "rock";
 let input2 = "paper";
 let input3 = "scissors";
+let result;
+
 
 if(playerSelection === input1) {
 
     if (computerSelection === input2) {
-
         // return alert("Paper beats Rock! You lose!"); 
-        return lose;
+        result = lose; 
+        /* rock_card.style.cssText = 'background-color: red';
+        paper_card.style.cssText = 'background-color: green'; */
+        selection_text.textContent = "Paper beats Rock! You lose. Try again!";
 
     } else if (computerSelection === input3) {
-
         // return alert("Rock beats Scissors! You win!");
-        return win;
+        result = win; 
+        /* rock_card.style.cssText = 'background-color: green';
+        scissors_card.style.cssText = 'background-color: red'; */
+        selection_text.textContent = "Rock beats Scissors! You win!"
 
-    } else return tie;
+    } else {
+        result = tie
+        /* rock_card.style.cssText = 'background-color: gray'; */
+        selection_text.textContent = "It's a tie! Keep trying...";
+        
+    }
 
 
 }else if (playerSelection === input2) {
 
     if (computerSelection === input1) {
-
         // return alert("Paper beats Rock! You win!");
-         return win;
-
+        result = win;
+        /* paper_card.style.cssText = 'background-color: green';
+        rock_card.style.cssText = 'background-color: red'; */
+        selection_text.textContent = "Paper beats Rock! You win!";
+        
     } else if (computerSelection === input3) {
-
     //  return alert("Scissors beat Paper! You Lose!");
-       return lose;
-
-    } else return tie;
+        /* paper_card.style.cssText = 'background-color: red';
+        scissors_card.style.cssText = 'background-color: green'; */
+        result = lose;
+        selection_text.textContent = "Scissors beat Paper! You Lose. Try again!";
+        
+    } else {
+        result = tie;
+        /* paper_card.style.cssText = 'background-color: gray'; */
+        selection_text.textContent = "It's a tie! Keep trying...";
+        
+    }
 
 
 } else if (playerSelection === input3) {
 
     if (computerSelection === input1) {
-
         // return alert("Rock beats Scissors! You lose!");
-        return lose;
-
+        result = lose;
+        /* rock_card.style.cssText = 'background-color: green';
+        scissors_card.style.cssText = 'background-color: red'; */
+        selection_text.textContent = "Rock beats Scissors! You lose. Try again!";
+        
     } else if (computerSelection === input2) {
-
         // return alert("Scissors beat Paper! You win!");
-         return win;
+        result = win;
+        /* paper_card.style.cssText = 'background-color: red';
+        scissors_card.style.cssText = 'background-color: green'; */
+        selection_text.textContent = "Scissors beat Paper! You win!";
+        
+    } else {
+        result = tie; 
+        /* scissors_card.style.cssText = 'background-color: gray'; */
+        selection_text.textContent = "It's a tie! Keep trying..."
+    }}
 
-    } else return tie;
-}}
+    console.log(result);
 
 
-btns.forEach(btn => btn.addEventListener('click', playRound));
+if (result === win) {
+    scorePlayer++;
+    player_points.textContent = scorePlayer;
 
-
-/* function game() {
-
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-
-
-    while (scoreComputer <= 5 || scorePlayer <= 5) {
-
-    let round = playRound();
-    let roundResult = round;
-
-    console.log("Round result = " + roundResult);
-
-    if (roundResult === win) {
-        scorePlayer++;
-        alert("You scored 1 point! Go again...");
-        // playRound();
-        console.log("Player = " + scorePlayer, "Computer = " + scoreComputer);
-    } else if (roundResult === lose) {
-        scoreComputer++;
-        alert("You lost! :( Try again!");
-        console.log("Player = " + scorePlayer, "Computer = " + scoreComputer);
-        // playRound();
-    } else if (roundResult === tie) {
-         alert("It's a tie! Try again!")
-         console.log("Player = " + scorePlayer, "Computer = " + scoreComputer);
-        //  playRound();
+} else if (result === lose) {
+    scoreComputer++;
+    computer_points.textContent = scoreComputer;
     }
 
-    if (scorePlayer === 5) {
-        alert("You won the game!");
-    } else if (scoreComputer === 5) {
-        alert("Game over! You lost :(");
-    }
 
-    if (scorePlayer === 5 || scoreComputer === 5) {
-        break;
-    }
-
-}
-
-console.log("Player = " + scorePlayer);
+console.log("player = " + scorePlayer);
 console.log("Computer = " + scoreComputer);
 
+
+if (scorePlayer === 5 || scoreComputer === 5) {
+    
+    if(scorePlayer === 5) {
+
+        selection_text.textContent = "You win!!!";
+        selection_text.style.color = 'green';
+        player_points.style.color = 'green';
+        label_player.style.color = 'green';
+
+    } else if(scoreComputer === 5) {
+        
+        selection_text.textContent = "You lose :(";
+        selection_text.style.color = 'red';
+        computer_points.style.color = 'red';
+        label_computer.style.color = 'red';
+    }
 }
 
-game(); */
+showModal();
+
+};
+/*  */
+
+
+/* ----------------------MODAL-------------------- */
+
+const modal = document.querySelector('.modal');
+/* const span = document.querySelector('.close'); */
+const modal_text = document.querySelector('.modal-text');
+const play_again = document.querySelector('.play-again');
+
+function showModal() {
+
+if (scoreComputer === 5) {
+    modal_text.textContent = "You lost. Click the button to play again!";
+    modal.style.display = "block";
+
+} else if (scorePlayer === 5) {
+    modal_text.textContent = "You won!! Click the button to play again!";
+    modal.style.display = "block";
+}
+
+play_again.addEventListener('click', () => {
+    modal.style.display = "none";
+    scoreComputer = 0;
+    scorePlayer = 0;
+
+    selection_text.textContent = "Click on a card to start playing";
+    selection_text.style.color = '#33332d';
+    computer_points.style.color = '#33332d';
+    player_points.style.color = '#33332d';
+    label_computer.style.color = '#33332d';
+    label_player.style.color = '#33332d';
+
+    computer_points.textContent = scoreComputer;
+    player_points.textContent = scorePlayer;
+
+})
+
+}
+
+
+
+
+
 
 
 
